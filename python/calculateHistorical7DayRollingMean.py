f31fbf6c3fdfd37450a3a62eb559491a9736286b
jebbss <jebsumerac@gmail.com>
2021-02-16 22:14:15 -0600

    data update

--- python/calculateHistorical7DayRollingMean.py
+++ python/calculateHistorical7DayRollingMean.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import os
 def calculateHistorical7DayRollingMeanJabs():
-    fieldsOfInterest = ['total_doses_daily', 'date', 'daily_doses_rolling']
+    fieldsOfInterest = ['total_doses_daily', 'date', 'daily_doses_rolling', 'days_to_complete']
     jsonFileNamePath = 'chicago-jab-data.json'
     if(not os.path.isfile(jsonFileNamePath)):
         print('oops')
@@ -9,6 +9,7 @@ def calculateHistorical7DayRollingMeanJabs():
     df = pd.read_json(jsonFileNamePath)
     df = df.reindex(index=df.index[::-1])
     df['daily_doses_rolling'] = df['total_doses_daily'].rolling(7).mean()
+    df['days_to_complete'] = 4310362 / df['daily_doses_rolling']
     df = df[fieldsOfInterest]
     df = df.reindex(index=df.index[::-1])
     df.to_json(path_or_buf='rolling-mean.json', orient='records')
