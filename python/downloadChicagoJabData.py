72c5915e17083ae6d7c8ed6e8c12ea3f656f0077
jebbss <jebsumerac@gmail.com>
2021-04-25 10:13:10 -0500

    data update

--- python/downloadChicagoJabData.py
+++ python/downloadChicagoJabData.py
@@ -3,8 +3,12 @@ import json
 
 def downloadChicagoJabData():
     response = requests.get('https://data.cityofchicago.org/resource/2vhs-cf6b.json')
+    jsonBody = response.json()
+    # The order of the response occasionally gets reversed
+    if("2020" in jsonBody[0]["date"]):
+        jsonBody.reverse()
     with open('chicago-jab-data.json', 'w') as f:
-        json.dump(response.json(), f)
+        json.dump(jsonBody, f)
 
 if __name__ == "__main__":
     downloadChicagoJabData()
